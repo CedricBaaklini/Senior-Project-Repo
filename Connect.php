5697fa3d48f41e4fb431ffa6a590ddbd57e6251b
CedricBaaklini <77132080+CedricBaaklini@users.noreply.github.com>
2022-03-07 15:27:07 -0500

    Renaming variables in the connection file

--- Connect.php
+++ Connect.php
@@ -1,14 +1,14 @@
 <?php
 
 $servername;
-$username;
-$password;
+$serverUsername;
+$serverPassword;
 
-$conn = newsqli($servername, $username, $password);
+$conn = newsqli($servername, $serverUsername, $serverPassword);
 
 if ($conn -> connect_error)
 {
   die ("Connection failed: " . $conn -> connect_error);
-} 
+}
 
 ?>
